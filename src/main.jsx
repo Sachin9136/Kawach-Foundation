e6c7f33a02b1e201816d08ea98671b212a627117
Sachin Gupta <9136167692@users.noreply.github.com>
2024-09-15 17:00:41 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { Provider } from "react-redux";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React>
 );
